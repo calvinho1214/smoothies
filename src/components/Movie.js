686c711740b63d1901769782f4fcdb02dac8ad37
calvin ho <calvinho1214@gmail.com>
2020-02-19 00:03:07 -0500

    movie component has movie img, title and year

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,15 @@ const Movie = ({ movie }) => {
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
     <div className="movie">
+    <h2>{movie.Title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The movie titled: ${movie.Title}`}
+          src={poster}
+        />
+      </div>
+      <p>({movie.Year})</p>
     </div>
   );
 };
