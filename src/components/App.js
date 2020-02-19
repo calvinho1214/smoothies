c603cc7d10df3ba74e2ecee6dac037e2318350c2
calvin ho <calvinho1214@gmail.com>
2020-02-19 00:42:48 -0500

    Search component to search movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Header from './Header'
+import Header from './Header';
+import Movie from './Movie';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
      <Header text="SMOOTHIES" />
+     <div className="movies">
+            <Movie />
+      </div>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Search = (props) => {
+
+  return (
+      <form className="search">
+      </form>
+    );
+}
+
+export default Search;
