c1622ccf48a170b962dde772253b0d5329bed5eb
calvin ho <calvinho1214@gmail.com>
2020-02-19 00:02:20 -0500

    adding movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg";
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+  return (
+    <div className="movie">
+    </div>
+  );
+};
+
+
+export default Movie;
