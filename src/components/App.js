de916ebeadcf551f84f46c3b3e914c8f6fa05d7a
calvin ho <calvinho1214@gmail.com>
2020-02-19 16:58:28 -0500

    adding state from movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Movie from './Movie';
+import Search from './Search'
 import '../App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   return (
     <div className="App">
      <Header text="SMOOTHIES" />
