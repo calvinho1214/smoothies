b0313546d4a97eee319b611f1ef1ad7e832fe18d
calvin ho <calvinho1214@gmail.com>
2020-02-19 00:43:47 -0500

    functions added to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,21 @@ import React from "react";
 
 
 const Search = (props) => {
+    const [searchValue, setSearchValue] = useState("");
+  
+    const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+    const resetInputField = () => {
+    setSearchValue("")
+  }
+
+    const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
 
   return (
       <form className="search">
